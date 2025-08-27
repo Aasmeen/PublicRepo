883afcd7923a98f50a83128d1c9a87051b35ce3b
Ahad <shawnfrost80@gmail.com>
2025-08-28 00:15:03 +0530

    Add album app

--- apps/album/models.py
+++ apps/album/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-from apps.core.models import TimeStampedModel
-
-
-class Album(TimeStampedModel):
-    title = models.CharField(max_length=200)
+# Create your models here.
