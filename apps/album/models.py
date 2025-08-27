563d6e443676c161f7c48661eced24311e8e51bd
Ahad <shawnfrost80@gmail.com>
2025-08-28 00:42:56 +0530

    Add album model

--- apps/album/models.py
+++ apps/album/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from apps.core.models import TimeStampedModel
+
 # Create your models here.
+
+class Album(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    release_date = models.DateTimeField(auto_now_add=True)
+
